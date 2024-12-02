3d54932af8060c7fe9b664ae5fa687317766f5b7
Brahim DRIOUCH <brahim.driouch1987@gmail.com>
2024-12-03 00:07:24 +0100

    appostrophies

--- app/disclaimer/page.tsx
+++ app/disclaimer/page.tsx
@@ -6,9 +6,9 @@ import React from 'react'
 function DisclaimerPage() {
   return (
     <div className='min-h-screen'>
-        Notre site peut inclure des publicités ou des liens d&aposaffiliation. Cela signifie que nous pouvons percevoir une commission lorsque vous cliquez sur ces liens ou effectuez un achat via ces derniers. Ces publicités et partenariats nous permettent de maintenir et de développer nos services tout en restant accessibles gratuitement pour les utilisateurs.
+        Notre site peut inclure des publicités ou des liens d&apos'affiliation. Cela signifie que nous pouvons percevoir une commission lorsque vous cliquez sur ces liens ou effectuez un achat via ces derniers. Ces publicités et partenariats nous permettent de maintenir et de développer nos services tout en restant accessibles gratuitement pour les utilisateurs.
 
-Nous tenons à préciser que les produits ou services promus via des publicités ou des affiliations sont choisis avec soin. Cependant, nous ne garantissons ni la qualité, ni la pertinence, ni la disponibilité de ces produits ou services. Toute décision d&aposachat ou d&aposutilisation relève de votre entière responsabilité.
+Nous tenons à préciser que les produits ou services promus via des publicités ou des affiliations sont choisis avec soin. Cependant, nous ne garantissons ni la qualité, ni la pertinence, ni la disponibilité de ces produits ou services. Toute décision d&apos'achat ou d&apos'utilisation relève de votre entière responsabilité.
 
 Merci de votre compréhension et de votre soutien à notre plateforme.
     </div>
