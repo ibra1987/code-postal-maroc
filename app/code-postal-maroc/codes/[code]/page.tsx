8fac7e673fa1127dd36718ffebfb17a064605d67
Brahim DRIOUCH <brahim.driouch1987@gmail.com>
2024-12-03 00:10:44 +0100

    mobile nav

--- app/code-postal-maroc/codes/[code]/page.tsx
+++ app/code-postal-maroc/codes/[code]/page.tsx
@@ -4,8 +4,8 @@ import { ChevronRight } from "lucide-react";
 
 export const revalidate = 6;
 
-// We&apos'll prerender only the params from `generateStaticParams` at build time.
-// If a request comes in for a path that hasn&apos't been generated,
+// We&apos;'ll prerender only the params from `generateStaticParams` at build time.
+// If a request comes in for a path that hasn&apos;'t been generated,
 // Next.js will server-render the page on-demand.
 export const dynamicParams = true; // or false, to 404 on unknown paths
 export async function generateStaticParams() {
