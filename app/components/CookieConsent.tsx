389417901cb7509ce8a303f6a64d05c77f4f1aad
Brahim DRIOUCH <brahim.driouch1987@gmail.com>
2024-12-03 23:46:21 +0100

    comment out unused code

--- app/components/CookieConsent.tsx
+++ app/components/CookieConsent.tsx
@@ -1,21 +1,21 @@
-"use client"
+// "use client"
 
-import Link from "next/link"
-import { useState } from "react"
+// import Link from "next/link"
+// import { useState } from "react"
 
-const CookieConsent = () => {
-    const [show,setShow]=  useState(true)
-  return (
-    <div className="w-full bg-red-500 text-white">
-    <p>
-    Nous utilisons des cookies pour améliorer votre expérience sur notre site. En continuant à naviguer, vous acceptez notre 
-    <Link className="font-medium underline" href="/politique-de-confidentialite">politique de confidentialité</Link> et l'utilisation des cookies.
-  </p>
-  <button onClick={()=>{}}>Accepter</button>
-  <button onClick={()=>setShow(false)}>Refuser</button>
-    </div>
+// const CookieConsent = () => {
+//     const [show,setShow]=  useState(true)
+//   return (
+//     <div className="w-full bg-red-500 text-white">
+//     <p>
+//     Nous utilisons des cookies pour améliorer votre expérience sur notre site. En continuant à naviguer, vous acceptez notre 
+//     <Link className="font-medium underline" href="/politique-de-confidentialite">politique de confidentialité</Link> et l'utilisation des cookies.
+//   </p>
+//   <button onClick={()=>{}}>Accepter</button>
+//   <button onClick={()=>setShow(false)}>Refuser</button>
+//     </div>
 
-  )
-}
+//   )
+// }
 
-export default CookieConsent
+// export default CookieConsent
