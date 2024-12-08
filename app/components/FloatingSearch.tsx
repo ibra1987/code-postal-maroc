fcf253b2e9338d11f02832434b4d12d9f451cae2
Brahim DRIOUCH <brahim.driouch1987@gmail.com>
2024-12-08 01:13:33 +0100

    search component changes

--- app/components/FloatingSearch.tsx
+++ app/components/FloatingSearch.tsx
@@ -25,18 +25,18 @@ function FloatingSearch({close}:{close:()=>void}) {
         setResult(filteredResults);
     }
   return (
-    <div className="min-h-screen w-screen mx-auto   flex flex-col justify-start items-center absolute top-0 left-0 bg-white z-100 p-4">
+    <div className="min-h-screen w-screen mx-auto bg-black/25 p-20   flex flex-col justify-start items-center absolute top-0 left-0 bg-white z-100 p-4">
         <div className="w-full text-sm hover:cursor-pointer flex justify-end items-center">
         <CircleX className="z-50  " color="red" onClick={close} />
          </div>
-        <form className="w-full lg:w-3/5">
+        <form className="w-full lg:w-4/5">
             <input autoFocus={true} onChange={showSearchResults} type="search" className="w-full border-b p-2 text-gray-600" placeholder="Taper votre recherche ici" />
         </form>
 
 
  <div className="w-full flex flex-col justify-start items-center">
   {result && result.length>0 &&(
-    <div className="w-full lg:w-3/5 items-start justify-center">
+    <div className="w-full lg:w-4/5 items-start justify-center">
         
 
         <div className="w-full grid grid-cols-4 justify-items-start text-white p-2 bg-red-500 ">
